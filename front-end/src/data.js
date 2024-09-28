1f6c511f30fa5432049673785d27789606ef63b7
archit batham <architbatham64@gmail.com>
2024-09-28 15:05:20 +0530

    first commit

---
+++ front-end/src/data.js
@@ -0,0 +1,6 @@
+let moviesList = ['Avenger Aomesday', 'Harry Potter', 'Fast X', 'Dardevil', 'Dilwale Dulhania Le Jayenge'];
+let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"];
+let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+exports.moviesList = moviesList;
+exports.slots = slots;
+exports.seats = seats;
